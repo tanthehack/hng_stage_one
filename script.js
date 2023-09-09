401b6bf14aefcd6f5daceb0ed12f44fcd468c2a3
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-09-10 00:25:07 +0100

    changes

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ let date = new Date();
 let currentDay = date.getDay();
 
 dayDiv.textContent = currentDay === 0 ? "Sunday" : currentDay === 1 ? "Monday" : currentDay === 2 ? "Tuesday" : currentDay === 3 ? "Wednesday" : currentDay === 4 ? "Thursday" : currentDay === 5 ? "Friday" : "Saturday";
-timeDiv.textContent = date.toLocaleTimeString();
+timeDiv.textContent = date.getTime();
