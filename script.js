40d0fdaf9acad7d37bbf1e9066e1de2c8db23802
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-09-10 09:15:35 +0100

    fix: time update

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const dayDiv = document.getElementById("currentDay");
 const timeDiv = document.getElementById("currentTime");
 
-let date = new Date();
-let currentDay = date.getDay();
 
-dayDiv.textContent = currentDay === 0 ? "Sunday" : currentDay === 1 ? "Monday" : currentDay === 2 ? "Tuesday" : currentDay === 3 ? "Wednesday" : currentDay === 4 ? "Thursday" : currentDay === 5 ? "Friday" : "Saturday";
-timeDiv.textContent = date.getTime();
+
+setInterval(setDate, 1);
+
+function setDate() {
+    let date = new Date();
+    let currentDay = date.getDay();
+    dayDiv.textContent = currentDay === 0 ? "Sunday" : currentDay === 1 ? "Monday" : currentDay === 2 ? "Tuesday" : currentDay === 3 ? "Wednesday" : currentDay === 4 ? "Thursday" : currentDay === 5 ? "Friday" : "Saturday";
+    timeDiv.textContent = date.getTime();
+}
+
